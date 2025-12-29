cfdaaf96ddd4104ba6e7d6536dd0c115de97518a
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-29 13:26:19 +0100

    feat: handle case where there's a tie

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -12,11 +12,13 @@ module TicTacToe
     def play
       starting_player = pick_random_starter
       puts "#{starting_player} will start the game!"
+      puts "Each turn you will pick your mark by selecting a number\n" \
+           "depicting one of the possible mark positions:"
+      puts board
       player_cycler = make_player_cycler(starting_player)
       while (board.nr_of_marks("x") + board.nr_of_marks("o")) != 9 do
         current_player = player_cycler.next
         puts "#{current_player}, where will you put your mark?"
-        puts board
         player_move = gets.chomp
         board.make_mark(current_player.mark, player_move)
         puts "New state of the board:"
@@ -26,6 +28,7 @@ module TicTacToe
           return
         end
       end
+      puts "Game ends with a tie."
     end
 
     private
